589a02504fb1d0bdd6fc3670b60454a5417590b7
Nicolae Postica <nicolae.postica2@gmail.com>
2020-10-21 13:17:42 +0300

    add flurry integration

--- android/app/src/main/java/com/freelancescanner/MainApplication.java
+++ android/app/src/main/java/com/freelancescanner/MainApplication.java
@@ -8,6 +8,8 @@ import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.flurry.android.FlurryAgent;
+
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
@@ -43,6 +45,9 @@ public class MainApplication extends Application implements ReactApplication {
   @Override
   public void onCreate() {
     super.onCreate();
+    new FlurryAgent.Builder()
+           .withLogEnabled(true)
+           .build(this, "GSCQ7S8FTRWN23GCR82K");
     SoLoader.init(this, /* native exopackage */ false);
     initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
   }
